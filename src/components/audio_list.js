d7c226a60793ea61af60eaca95da2ad39f4faf09
choiharam <altitudinem@gmail.com>
2024-02-04 23:23:39 +0100

    -

--- src/components/audio_list.js
+++ src/components/audio_list.js
@@ -1,85 +1,85 @@
 const audio = [
     {
-        category: "0-category",
+        category: "category0 - DE",
         audios: [
             {
-                name: "0-audio0",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/key1.mp3"
+                name: "0-DE-0",
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/de0.mp3"
             },
             {
-                name: "0-audio1",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/key2.mp3"
+                name: "0-DE-1",
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/de1.mp3"
             },
             {
-                name: "0-audio2",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/key3.mp3"
+                name: "0-DE-2",
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/de2.mp3"
             },
             {
-                name: "0-audio3",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/key4.mp3"
+                name: "0-DE-3",
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/de3.mp3"
             }
         ],
     },
     {
-        category: "1-category",
+        category: "category1 - KR",
         audios: [
             {
-                name: "1-audio0",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/key1.mp3"
+                name: "1-KR-0",
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/ko0.mp3"
             },
             {
-                name: "1-audio1",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/key2.mp3"
+                name: "1-KR-1",
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/ko1.mp3"
             },
             {
-                name: "1-audio2",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/key3.mp3"
+                name: "1-KR-2",
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/ko2.mp3"
             },
             {
-                name: "1-audio3",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/key4.mp3"
+                name: "1-KR-3",
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/ko3.mp3"
             }
         ],
     },
     {
-        category: "2-category",
+        category: "category2 - ES",
         audios: [
             {
-                name: "2-audio0",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/key1.mp3"
+                name: "2-ES-0",
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/es0.mp3"
             },
             {
-                name: "2-audio1",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/key2.mp3"
+                name: "2-ES-1",
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/es1.mp3"
             },
             {
-                name: "2-audio2",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/key3.mp3"
+                name: "2-ES-2",
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/es2.mp3"
             },
             {
-                name: "2-audio3",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/key4.mp3"
+                name: "2-ES-3",
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/es3.mp3"
             }
         ],
     },
     {
-        category: "3-category",
+        category: "category3 - EN",
         audios: [
             {
-                name: "3-audio0",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/key1.mp3"
+                name: "3-EN-0",
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/en0.mp3"
             },
             {
-                name: "3-audio1",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/key2.mp3"
+                name: "3-EN-1",
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/en1.mp3"
             },
             {
-                name: "3-audio2",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/key3.mp3"
+                name: "3-EN-2",
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/en2.mp3"
             },
             {
-                name: "3-audio3",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/key4.mp3"
+                name: "3-EN-3",
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/en3.mp3"
             }
         ],
     }
