36559757f034c2821d011bd12e43fb5207f7a0ae
choiharam <altitudinem@gmail.com>
2024-02-04 23:25:32 +0100

    -

--- src/components/audio_list.js
+++ src/components/audio_list.js
@@ -4,19 +4,19 @@ const audio = [
         audios: [
             {
                 name: "0-DE-0",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/de0.mp3"
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/de0.wav"
             },
             {
                 name: "0-DE-1",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/de1.mp3"
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/de1.wav"
             },
             {
                 name: "0-DE-2",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/de2.mp3"
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/de2.wav"
             },
             {
                 name: "0-DE-3",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/de3.mp3"
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/de3.wav"
             }
         ],
     },
@@ -25,19 +25,19 @@ const audio = [
         audios: [
             {
                 name: "1-KR-0",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/ko0.mp3"
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/ko0.wav"
             },
             {
                 name: "1-KR-1",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/ko1.mp3"
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/ko1.wav"
             },
             {
                 name: "1-KR-2",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/ko2.mp3"
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/ko2.wav"
             },
             {
                 name: "1-KR-3",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/ko3.mp3"
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/ko3.wav"
             }
         ],
     },
@@ -46,19 +46,19 @@ const audio = [
         audios: [
             {
                 name: "2-ES-0",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/es0.mp3"
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/es0.wav"
             },
             {
                 name: "2-ES-1",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/es1.mp3"
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/es1.wav"
             },
             {
                 name: "2-ES-2",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/es2.mp3"
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/es2.wav"
             },
             {
                 name: "2-ES-3",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/es3.mp3"
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/es3.wav"
             }
         ],
     },
@@ -67,19 +67,19 @@ const audio = [
         audios: [
             {
                 name: "3-EN-0",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/en0.mp3"
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/en0.wav"
             },
             {
                 name: "3-EN-1",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/en1.mp3"
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/en1.wav"
             },
             {
                 name: "3-EN-2",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/en2.mp3"
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/en2.wav"
             },
             {
                 name: "3-EN-3",
-                src: "https://haramchoi-hfk.github.io/idm_media_hosting/en3.mp3"
+                src: "https://haramchoi-hfk.github.io/idm_media_hosting/en3.wav"
             }
         ],
     }
